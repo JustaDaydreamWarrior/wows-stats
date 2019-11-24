858d52cb4ddf791b476ec273aa10dedbe13791cd
Jason Keen <s3591176@student.rmit.edu.au>
2019-11-24 14:58:37 +1100

    Update install.js
    Added support for WorldOfWarships64.exe

--- install.js
+++ install.js
@@ -1,8 +1,8 @@
 require('dotenv').load();
-var express 	= require('express');
-var bodyParser 	= require('body-parser');
-var fs			= require('fs');
-var request 	= require('request');
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var request = require('request');
 
 var app = express();
 var port = process.env.PORT || 8080;
@@ -17,7 +17,7 @@ app.use('/api', router);
 // create application/json parser 
 var jsonParser = bodyParser.json();
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
 	res.json({
 		status: "ok",
 		name: "wows-stats installer api",
@@ -25,7 +25,7 @@ router.get('/', function(req, res) {
 	});
 });
 
-router.get('/env', function(req, res) {
+router.get('/env', function (req, res) {
 	var env = {};
 	if (process.env.WOWS_PATH)
 		env.path = process.env.WOWS_PATH;
@@ -36,9 +36,9 @@ router.get('/env', function(req, res) {
 	res.json(env);
 });
 
-router.post('/path', jsonParser, function(req, res) {
+router.post('/path', jsonParser, function (req, res) {
 	if (req.body.path) {
-		fs.access(req.body.path + "/WorldOfWarships.exe", fs.R_OK, function (err) {
+		fs.access(req.body.path + "/WorldOfWarships64.exe", fs.R_OK, function (err) {
 			if (!err)
 				res.sendStatus(200);
 			else
@@ -49,23 +49,23 @@ router.post('/path', jsonParser, function(req, res) {
 		res.sendStatus(400);
 });
 
-router.post('/install', jsonParser, function(req, res) {
+router.post('/install', jsonParser, function (req, res) {
 	if (req.body.action) {
 		if (req.body.action == "cancel") {
 			res.sendStatus(200);
 			console.log("User cancelled wows-stats installation.")
 			process.exit(1);
 		}
-		else if(req.body.action == "save") {
+		else if (req.body.action == "save") {
 
 			var validation = {};
 
-			validation.checkPath = function() {
+			validation.checkPath = function () {
 				if (req.body.path) {
-					fs.access(req.body.path + "/WorldOfWarships.exe", fs.R_OK, function (err) {
+					fs.access(req.body.path + "/WorldOfWarships64.exe", fs.R_OK, function (err) {
 						if (err)
 							return res.status(400).send("World Of Warships not found.");
-						else 
+						else
 							validation.checkKey();
 					});
 				}
@@ -73,7 +73,7 @@ router.post('/install', jsonParser, function(req, res) {
 					return res.sendStatus(400);
 			}
 
-			validation.checkKey = function() {
+			validation.checkKey = function () {
 				if (req.body.key) {
 					request(req.body.url + '/wows/encyclopedia/info/?application_id=' + req.body.key, function (error, response, body) {
 						if (!error && response.statusCode == 200) {
@@ -91,22 +91,22 @@ router.post('/install', jsonParser, function(req, res) {
 					return validation.save();
 			}
 
-			validation.save = function() {
-				fs.writeFile('.env', 
+			validation.save = function () {
+				fs.writeFile('.env',
 					'WOWS_PATH="' + req.body.path + '"\n' +
-					'WOWS_API_URL="' + req.body.url + '"\n' + 
-					(req.body.key ? ('WOWS_API_KEY=' + req.body.key):''),
+					'WOWS_API_URL="' + req.body.url + '"\n' +
+					(req.body.key ? ('WOWS_API_KEY=' + req.body.key) : ''),
 					function (err) {
-					  	if (!err) {
-					  		res.sendStatus(200);
-					  		process.exit(0);
-					  		return;
-					  	}
-					  	else {
-					  		console.log(err);
-					  		return res.sendStatus(500);
-					  	}
-				});
+						if (!err) {
+							res.sendStatus(200);
+							process.exit(0);
+							return;
+						}
+						else {
+							console.log(err);
+							return res.sendStatus(500);
+						}
+					});
 			}
 
 			validation.checkPath();
